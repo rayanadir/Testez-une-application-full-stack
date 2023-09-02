364d717a2ab751cb7fe486748d7cd5c5f1a0512b
rayanadir <rayan.dahmena@gmail.com>
2023-09-02 19:43:28 +0200

    add first unit test

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,6 +15,12 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+
+  // add login component constructor parameters (mocks)
+  let mockAuthService = jest.fn().mockReturnValue(jest.fn());
+  let mockFormBuilder: FormBuilder = new FormBuilder();
+  let mockRouter = jest.fn();
+  let mockSessionService = jest.fn();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -39,4 +45,42 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("Login successful", () => {
+    // Login request data
+    const loginReq = {
+      email:"yoga@studio.com",
+      password:"test!12345"
+    }
+    
+    // Expected login response data
+    const loginRes = {
+      token:"token",
+      type:"Bearer",
+      id:1,
+      username:"username",
+      firstName:"firstName",
+      lastName:"lastName",
+      admin:true,
+    }
+
+    // Input & submit form
+    component.form.setValue(loginReq);
+    component.submit();
+
+    // Call authService with login request data
+    expect(mockAuthService).toHaveBeenCalledTimes(1);
+    expect(mockAuthService).toHaveBeenCalledWith(loginReq);
+
+    // Get authService response
+    mockAuthService.mockReturnValue(loginRes);
+
+    // Call sessionService with login response data
+    expect(mockSessionService).toHaveBeenCalledTimes(1);
+    expect(mockSessionService).toHaveBeenCalledWith(loginRes);
+
+    // Call navigation
+    expect(mockRouter).toHaveBeenCalledTimes(1);
+    expect(mockRouter).toHaveBeenCalledWith(['/sessions']);
+  })
 });
