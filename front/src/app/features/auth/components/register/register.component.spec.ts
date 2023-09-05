e005969686f40a7e9c8a2939b28adf62b4d6290a
rayanadir <rayan.dahmena@gmail.com>
2023-09-05 16:14:14 +0200

    add invalid register test

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -6,9 +6,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { expect } from '@jest/globals';
+//import { describe, expect } from '@jest/globals';
+import { JestExpect } from '@jest/expect';
+import { throwError } from 'rxjs';
 
 import { RegisterComponent } from './register.component';
+import { describe } from '@jest/globals';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+declare const expect: JestExpect;
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -37,4 +44,32 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe("Register", () => {
+    let mockAuthService: any = {
+      register: jest.fn().mockReturnValue({subscribe:jest.fn()})
+    }; 
+    let mockFormBuilder: FormBuilder = new FormBuilder();
+    let mockRouter: any = {navigate:jest.fn()};
+
+    let mockComponent: RegisterComponent = new RegisterComponent(mockAuthService as AuthService,mockFormBuilder,mockRouter as Router)
+    
+    it("Register failed, invalid fields", () => {
+      const registerReq= {
+        email:"",
+        firstName:"",
+        lastName:"",
+        password:""
+      }
+      mockComponent.form.setValue(registerReq);
+      mockAuthService.register.mockReturnValue(throwError(() => {new Error("Error : invalid fields !")}));
+     
+      mockComponent.submit();
+      
+      expect(mockComponent.onError).toBeTruthy();
+      expect(mockRouter.navigate).not.toHaveBeenCalledWith();
+    })
+  })
+
+
 });
