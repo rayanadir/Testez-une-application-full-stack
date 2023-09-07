c18768739d31836f9cd954ab3956039a4c607344
rayanadir <rayan.dahmena@gmail.com>
2023-09-07 23:03:52 +0200

    get user data test

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -4,10 +4,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
+import { expect, jest } from '@jest/globals';
 
 import { MeComponent } from './me.component';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/interfaces/user.interface';
+import { of } from 'rxjs';
 
 describe('MeComponent', () => {
   let component: MeComponent;
@@ -42,4 +47,45 @@ describe('MeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe("Me", () => {
+    // Add me component constructor parameters (mocks), object keys are service methods
+    let mockRouter : any;
+    let mockMatSnackBar! : MatSnackBar;
+    let mockUserService : any = {
+      getById: jest.fn().mockReturnValue({subscribe: jest.fn()}),
+      //getById: jest.fn().mockImplementation(() => mockUserID),
+    };
+
+    let mockComponent : MeComponent =  new MeComponent(
+      mockRouter as Router,
+      mockSessionService as SessionService,
+      mockMatSnackBar as MatSnackBar,
+      mockUserService as UserService
+    )
+
+    it("Displays user data" , () => {
+      let user: User | undefined;
+
+      // Expected user information data
+      user = {
+        id: 1,
+        email: "email@email.com",
+        lastName: "firstName",
+        firstName: "lastName",
+        admin: true,
+        password: "password",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+
+      mockComponent.ngOnInit();
+      
+      expect(mockUserService.getById).toHaveBeenCalledTimes(1);
+      expect(mockUserService.getById).toHaveBeenCalledWith(mockSessionService.sessionInformation.id.toString());
+      
+      mockUserService.getById.mockReturnValue(of(user));
+      
+    })
+  })
 });
