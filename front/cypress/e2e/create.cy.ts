7ebdcb8e696af591ea94e2c8bf87d4c0c59833ee
rayanadir <rayan.dahmena@gmail.com>
2023-09-15 18:47:05 +0200

    add empty fields creation test

--- front/cypress/e2e/create.cy.ts
+++ front/cypress/e2e/create.cy.ts
@@ -111,5 +111,89 @@ describe("Create spec", () => {
 
         cy.get("button[type=submit]").click();
 
+    });
+
+    it("No creation, empty form fields, disabled submit button", () => {
+        // Login mock
+        cy.visit('/login');
+
+        cy.intercept('POST', '/api/auth/login', {
+            body: {
+                id: 1,
+                username: "yoga@studio.com",
+                firstName: 'Admin',
+                lastName: 'Admin',
+                admin: true
+            }
+        });
+
+        let sessions = [
+            {
+                id: 1,
+                name: "session 1",
+                description: "my description",
+                date: "2012-01-01 01:00:00",
+                teacher_id: 1,
+                users: [],
+                createdAt: "2023-09-08 18:45:03",
+                updatedAt: "2023-09-12 23:23:22",
+            },
+            {
+                id: 2,
+                name: "Session",
+                description: "description",
+                date: "2023-09-12 02:00:00",
+                teacher_id: 2,
+                users: [],
+                createdAt: "2023-09-12 23:13:47",
+                updatedAt: "2023-09-12 23:13:47",
+            }
+        ]
+
+        cy.intercept("GET", '/api/session', {
+            body: sessions,
+        });
+
+        const teachers = [
+            {
+                id: 1,
+                lastName: "DELAHAYE",
+                firstName: "Margot",
+                createdAt: "2023-08-29 18:57:01",
+                updatedAt: "2023-08-29 18:57:01",
+            },
+            {
+                id: 2,
+                lastName: "THIERCELIN",
+                firstName: "Hélène",
+                createdAt: "2023-08-29 18:57:01",
+                updatedAt: "2023-08-29 18:57:01",
+            },
+        ];
+
+        cy.intercept('GET', '/api/teacher', teachers);
+
+        cy.get('input[formControlName=email]').type("yoga@studio.com");
+        cy.get('input[formControlName=password]').type(`${"test!12345"}{enter}{enter}`);
+
+        cy.get('.mat-raised-button').should("be.enabled");
+        cy.url().should('include', '/sessions');
+
+        cy.contains('Create').click();
+
+        cy.url().should('include', '/sessions/create');
+
+        const sessionForm = {
+            name:"",
+            date:"",
+            teacher:"",
+            description:"",
+        }
+
+        cy.get("input[formControlName=name]").should("have.value",sessionForm.name);
+        cy.get("input[formControlName=date]").should("have.value",sessionForm.date);
+
+        cy.get("textarea[formControlName=description]").should("have.value",sessionForm.description);
+        cy.get("button[type=submit]").should("be.disabled")
     })
 })
