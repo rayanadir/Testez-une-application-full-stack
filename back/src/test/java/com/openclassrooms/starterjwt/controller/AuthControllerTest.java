b13859fee8f55e1c277acf0ceae31c401832d7de
rayanadir <rayan.dahmena@gmail.com>
2023-09-18 17:19:48 +0200

    add mocks & @BeforeEach setUp

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controller/AuthControllerTest.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.starterjwt.controller;
+
+import com.openclassrooms.starterjwt.controllers.AuthController;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthControllerTest {
+
+    @InjectMocks
+    private AuthController authController;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private JwtUtils jwtUtils;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        authController = new AuthController(
+                authenticationManager,
+                passwordEncoder,
+                jwtUtils,
+                userRepository
+        );
+    }
+}
