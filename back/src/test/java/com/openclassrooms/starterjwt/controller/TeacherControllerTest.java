8ca8a47e374001f88feb5060ffa0c20039661c8e
rayanadir <rayan.dahmena@gmail.com>
2023-09-25 16:51:04 +0200

    add findById method, return response entity ok test

--- back/src/test/java/com/openclassrooms/starterjwt/controller/TeacherControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controller/TeacherControllerTest.java
@@ -1,13 +1,21 @@
 package com.openclassrooms.starterjwt.controller;
 
 import com.openclassrooms.starterjwt.controllers.TeacherController;
+import com.openclassrooms.starterjwt.dto.TeacherDto;
 import com.openclassrooms.starterjwt.mapper.TeacherMapper;
+import com.openclassrooms.starterjwt.models.Teacher;
 import com.openclassrooms.starterjwt.services.TeacherService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TeacherControllerTest {
@@ -24,5 +32,19 @@ public class TeacherControllerTest {
     @BeforeEach
     public void setUp(){
         teacherController = new TeacherController(teacherService, teacherMapper);
+    }
+
+    @Test
+    @DisplayName("findById method, return response entity ok")
+    void whenTeacherId_thenReturnResponseEntityOK(){
+        String id = "1";
+        Teacher teacher = new Teacher();
+        TeacherDto teacherDto = new TeacherDto();
+        when(teacherService.findById(Long.valueOf(id))).thenReturn(teacher);
+        when(teacherMapper.toDto(teacher)).thenReturn(teacherDto);
+        ResponseEntity<?> responseEntity = ResponseEntity.ok().body(teacherDto);
+        ResponseEntity<?> findById = teacherController.findById(id);
+        assertEquals(findById,responseEntity);
+        verify(teacherMapper, times(1)).toDto(teacher);
     }
 }
