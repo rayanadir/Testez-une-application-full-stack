b94196393ae7d8ee0cab95ce68ceba3961bdae3b
rayanadir <rayan.dahmena@gmail.com>
2023-10-02 16:30:20 +0200

    add integration tests

---
+++ back/src/test/java/com/openclassrooms/starterjwt/integration/SessionControllerIntegrationTest.java
@@ -0,0 +1,76 @@
+package com.openclassrooms.starterjwt.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SessionControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    ObjectMapper om = new ObjectMapper();
+
+    @Before("")
+    public void setUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    @DisplayName("findById method, response entity ok")
+    public void testFindById_responseEntityOK() throws Exception{
+        String email = "email@email.com";
+        String password = "password";
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email,password));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtUtils.generateJwtToken(authentication);
+        this.mockMvc
+                .perform(
+                        MockMvcRequestBuilders
+                                .get("/api/session/")
+                                .param("id","1")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    /*@Test
+    @DisplayName("findAll method, response entity ok")
+    public void testFindAll_responseEntityOK() throws Exception {
+        String email = "email@email.com";
+        String password = "password";
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email,password));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtUtils.generateJwtToken(authentication);
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/session").header(HttpHeaders.AUTHORIZATION, "Bearer " + token).contentType(APPLICATION_JSON)).andExpect(status().isOk());
+    }*/
+}
