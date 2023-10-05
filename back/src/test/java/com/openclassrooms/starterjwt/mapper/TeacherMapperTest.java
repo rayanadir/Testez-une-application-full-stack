80d5fcc225a0b6593043e514e5dc4ad34013c022
rayanadir <rayan.dahmena@gmail.com>
2023-10-05 18:33:17 +0200

    add test teacherDtoListToTeacherEntityList & teacherEntityListToTeacherDtoList mapper

--- back/src/test/java/com/openclassrooms/starterjwt/mapper/TeacherMapperTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/mapper/TeacherMapperTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -38,5 +41,35 @@ public class TeacherMapperTest {
 
         assertNotNull(teacherDto);
         assertEquals(teacherDto.getId(),teacher.getId());
+    }
+
+    @Test
+    void teacherDtoListToTeacherEntityList(){
+        TeacherDto teacherDto = new TeacherDto();
+        teacherDto.setId(3L);
+        teacherDto.setFirstName("firstname");
+        teacherDto.setLastName("lastname");
+
+        List<TeacherDto> teacherDtoList = new ArrayList<>();
+        teacherDtoList.add(teacherDto);
+
+        List<Teacher> teacherList = teacherMapper.toEntity(teacherDtoList);
+        assertNotNull(teacherList);
+        assertEquals(teacherDtoList.get(0).getId(), teacherList.get(0).getId());
+    }
+
+    @Test
+    void teacherEntityListToTeacherDtoList(){
+        Teacher teacher = new Teacher();
+        teacher.setId(3L);
+        teacher.setFirstName("firstname");
+        teacher.setLastName("lastname");
+
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(teacher);
+
+        List<TeacherDto> teacherDtoList = teacherMapper.toDto(teacherList);
+        assertNotNull(teacherDtoList);
+        assertEquals(teacherList.get(0).getId(), teacherDtoList.get(0).getId());
     }
 }
