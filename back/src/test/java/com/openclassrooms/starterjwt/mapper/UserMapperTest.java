601d873d138e1882e6e04a33862eede8aee93ba0
rayanadir <rayan.dahmena@gmail.com>
2023-10-05 17:45:27 +0200

    add test userDtoListToUserEntityList & userEntityListToUserDtoList mapper

--- back/src/test/java/com/openclassrooms/starterjwt/mapper/UserMapperTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/mapper/UserMapperTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -49,5 +52,31 @@ public class UserMapperTest {
         UserDto userDto = userMapper.toDto(userEntity);
         assertNotNull(userDto);
         assertEquals(userEntity.getId(),userDto.getId());
+    }
+
+    @Test
+    void userDtoListToUserEntityList(){
+        UserDto userDto = new UserDto();
+        userDto.setEmail("email@email.com");
+        userDto.setId(7L);
+        userDto.setAdmin(false);
+        userDto.setFirstName("firstname");
+        userDto.setLastName("lastname");
+        userDto.setPassword("password");
+        List<UserDto> userDtoList = new ArrayList<>();
+        userDtoList.add(userDto);
+
+        List<User> userList = userMapper.toEntity(userDtoList);
+        assertNotNull(userList);
+        assertEquals(userDtoList.get(0).getId(),userList.get(0).getId());
+    }
+
+    @Test
+    void userEntityListToUserDtoList(){
+        List<User> userList = new ArrayList<>();
+        userList.add(userEntity);
+        List<UserDto> userDtoList = userMapper.toDto(userList);
+        assertNotNull(userDtoList);
+        assertEquals(userList.get(0).getId(),userDtoList.get(0).getId());
     }
 }
